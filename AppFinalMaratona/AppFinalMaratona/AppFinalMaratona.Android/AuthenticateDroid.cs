ba1b7608c85ddf9d7b5364933bde6cf5031f8d94
Rafael Lillo <rafa_lillo@outlook.com>
2017-05-29 21:46:28 -0300

    Finishi autenticação

---
+++ AppFinalMaratona/AppFinalMaratona/AppFinalMaratona.Android/AuthenticateDroid.cs
@@ -0,0 +1,24 @@
+﻿using AppFinalMaratona.Droid;
+using AppFinalMaratona.Interface;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AuthenticateDroid))]
+namespace AppFinalMaratona.Droid
+{
+    public class AuthenticateDroid : IAuthenticate
+    {
+        public async Task<MobileServiceUser> Authenticate(MobileServiceClient client, MobileServiceAuthenticationProvider provider)
+        {
+            try
+            {
+                return await client.LoginAsync(Forms.Context, provider);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
